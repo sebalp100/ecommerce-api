f5a100ddeedd41eb01177aca75df436150fb9338
sebalp100 <sebalp100@gmail.com>
2024-02-26 19:54:44 -0300

    Add product model

--- models/User.js
+++ models/User.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin: { type: boolean, default: false },
+    isAdmin: { type: Boolean, default: false },
   }, {
   timestamps: true,
 }
 );
 
-module.exports = mongoose.Model("User", userSchema);
+module.exports = mongoose.Model("User", UserSchema);
